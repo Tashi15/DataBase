82a503e0322eb80c6cc9dcd84f9cad5101fcd9a9
Tashi15 <Tashi.Palden1@marist.edu>
2016-04-26 21:39:02 -0400

    Lab 10
    This is the answer for first question. I am working on second question

---
+++ Lab 10/Lab 10.sql
@@ -0,0 +1,24 @@
+﻿--- lAB 11-----
+
+--1. Funtion PreRegsFor(courseNum) - Returns the immediate prerequisites for the passed-in course number.
+
+
+create or replace function preRegsFor(int, REFCURSOR) returns refcursor as 
+$$
+declare
+   preRegNum int       := $1;
+   resultset REFCURSOR := $2;
+begin
+   open resultset for 
+      select *
+      from  Prerequisites
+     order by courseNum desc;
+   return resultset;
+end;
+$$ 
+language plpgsql;
+
+select preRegsFor(0, 'results');
+Fetch all from results;
+
+
